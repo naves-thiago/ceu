68591395a707a18d94edad5231f6f90ba3f179ba
Thiago Naves <naves.thiago@gmail.com>
2016-04-08 21:18:50 -0300

    Added support for ChibiOS threads

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	./ceu $(CEUFILE)
+	perl -p -i -e 's/line ([0-9]+)\.0/line $$1/g' _ceu_app.c
 	gcc main.c $(CFLAGS)
 
 clean:
--- ceu_threads.h
+++ ceu_threads.h
@@ -7,6 +7,7 @@ Not using yield anymore
 #define _GNU_SOURCE
 #endif
 
+#if 0
 #include <pthread.h>
 
 #define CEU_THREADS_T               pthread_t
@@ -16,6 +17,19 @@ Not using yield anymore
 #define CEU_THREADS_DETACH(t)       pthread_detach(t)
 #define CEU_THREADS_MUTEX_LOCK(m)   pthread_mutex_lock(m)
 #define CEU_THREADS_MUTEX_UNLOCK(m) pthread_mutex_unlock(m);
+#endif
+
+#include <ChibiOS_AVR.h>
+
+#define CEU_THREADS_T               thread_t
+#define CEU_THREADS_MUTEX_T         mutex_t
+#define CEU_THREADS_SELF()          chThdGetSelfX()
+#define CEU_THREADS_CREATE(t,f,p)   chThdCreateFromHeap(NULL, 128, NORMALPRIO, f, p)
+#define CEU_THREADS_DETACH(t)
+#define CEU_THREADS_MUTEX_LOCK(m)   chMtxLock(m)
+#define CEU_THREADS_MUTEX_UNLOCK(m) chMtxUnlock(m);
+
+
 /*
 #define CEU_THREADS_COND_T          pthread_cond_t
 #define CEU_THREADS_YIELD()         assert(sched_yield()==0)
